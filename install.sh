20bc677e2ae4c543b83c20f2a0087e0f68ac9057
root <lohityapushkar@gmail.com>
2018-09-29 11:01:00 +0530

    updated install script

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ echo
 echo '[!] Installing Dependencies...'
 echo '    Python'
 apt-get -y install python &>> install.log
-echo '    Apache2'
-apt-get -y install apache2 &>> install.log
 echo '    PHP'
 apt-get -y install php &>> install.log
 echo '    wget'
@@ -15,17 +13,12 @@ apt-get -y install unzip &>> install.log
 echo '    Requests'
 pip install requests &>> install.log
 echo
-echo '[!] Copying Template to /var/www/html/'
-rm -rf /var/www/html/nearyou/
-cp -r template/nearyou/ /var/www/html/
-echo
 echo '[!] Creating seeker symlink...'
-echo
 ln -s $PWD/seeker.py /usr/local/bin/seeker
 chmod 777 /usr/local/bin/seeker
 echo
 echo '[!] Setting Permissions...'
-chmod 777 /var/www/html/nearyou/php/info.txt
-chmod 777 /var/www/html/nearyou/php/result.txt
+chmod 777 template/nearyou/php/info.txt
+chmod 777 template/nearyou/php/result.txt
 echo
 echo '[!] Installed...Launch by Typing seeker'
