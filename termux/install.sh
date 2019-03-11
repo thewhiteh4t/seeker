ebcd3bf4558e9b13edc28c30a62d19b4b3e4abc5
root <lohityapushkar@gmail.com>
2019-03-11 23:30:29 +0530

    Removed Ngrok

--- termux/install.sh
+++ termux/install.sh
@@ -11,24 +11,10 @@ echo '    wget'
 apt-get -y install wget &>> install.log
 echo '    unzip'
 apt-get -y install unzip &>> install.log
+echo '    openssh'
+apt-get -y install openssh &>> install.log
 echo '    Requests'
 pip install requests &>> install.log
-echo
-echo '[!] Downloading Ngrok...'
-case `dpkg --print-architecture` in
-aarch64 | arm64)
-  wget https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm64.zip;
-  unzip ngrok-2.2.8-linux-arm64.zip;
-  mv ngrok $PREFIX/bin;
-  rm ngrok-2.2.8-linux-arm64.zip ;;
-armhf | armv7l | arm)
-  wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip;
-  unzip ngrok-stable-linux-arm.zip;
-  mv ngrok $PREFIX/bin;
-  rm ngrok-stable-linux-arm.zip ;;
-*)
-  echo Error : Unable to get Architecture! ;;
-esac
 echo
 echo '[!] Setting Permissions...'
 chmod 777 ../template/nearyou/php/result.txt
