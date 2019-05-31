e0123be5abfcb77c47f8700123f928aab2240254
thewhiteh4t <lohityapushkar@gmail.com>
2019-05-31 09:56:23 +0530

    Updated install script
    Former-commit-id: 8fcaffd1170dcd07c555d82b68cf1d881305feae

--- install.sh
+++ install.sh
@@ -2,18 +2,18 @@ echo '[!] Updating...'
 apt-get update > install.log
 echo
 echo '[!] Installing Dependencies...'
-echo '    Python'
-apt-get -y install python &>> install.log
+echo '    Python3'
+apt-get -y install python3 python3-pip &>> install.log
 echo '    PHP'
 apt-get -y install php &>> install.log
 echo '    wget'
 apt-get -y install wget &>> install.log
 echo '    unzip'
 apt-get -y install unzip &>> install.log
-echo '    openssh'
+echo '    ssh'
 apt-get -y install ssh &>> install.log
 echo '    Requests'
-pip install requests &>> install.log
+pip3 install requests &>> install.log
 echo
 echo '[!] Setting Permissions...'
 chmod 777 template/nearyou/php/info.txt
