5d9e728770426bb571eda4de9b3c89497dcf5bf7
thewhiteh4t <lohityapushkar@gmail.com>
2019-06-05 12:17:58 +0530

    Added termux install script
    Former-commit-id: e4597a1f729af06a143089ee0fafbe1974b51bfe

---
+++ termux_install.sh
@@ -0,0 +1,18 @@
+echo '[!] Updating...'
+apt-get update > install.log
+echo
+echo '[!] Installing Dependencies...'
+echo '    Python3'
+apt-get -y install python &>> install.log
+echo '    PHP'
+apt-get -y install php &>> install.log
+echo '    ssh'
+apt-get -y install openssh &>> install.log
+echo '    Requests'
+pip3 install requests &>> install.log
+echo
+echo '[!] Setting Permissions...'
+chmod 777 template/nearyou/php/info.txt
+chmod 777 template/nearyou/php/result.txt
+echo
+echo '[!] Installed.'
