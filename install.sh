7d5c8ed9c7c35f7cd15ad95ab7480abf1f6c55b6
mukesh kumar <mukeshkumarcharak@gmail.com>
2021-05-03 07:53:00 +0530

    Update install.sh
    'sudo' added for smooth installation.

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 echo '[!] Updating...'
-apt-get update > install.log
+sudo apt-get update > install.log
 echo
 echo '[!] Installing Dependencies...'
 echo '    Python3'
-apt-get -y install python3 python3-pip &>> install.log
+sudo apt-get -y install python3 python3-pip &>> install.log
 echo '    PHP'
-apt-get -y install php &>> install.log
+sudo apt-get -y install php &>> install.log
 echo '    ssh'
-apt-get -y install ssh &>> install.log
+sudo apt-get -y install ssh &>> install.log
 echo '    Requests'
-pip3 install requests &>> install.log
+sudo pip3 install requests &>> install.log
 echo
 echo '[!] Setting Permissions...'
 chmod 777 template/nearyou/php/info.txt
