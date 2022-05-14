b39341af50ffb7f9afda383664463b2c64e2d9eb
thewhiteh4t <lohityapushkar@gmail.com>
2022-05-14 14:52:46 +0530

    install script updated

--- install.sh
+++ install.sh
@@ -2,34 +2,65 @@
 
 ILOG=$PWD/logs/install.log
 
+status_check() {
+    if [ $? -eq 0 ]
+    then
+        echo -e "$1 - Installed"
+    else
+        echo -e "$1 - Failed!"
+    fi
+}
+
 debian_install() {
-    echo -e '=====================\nINSTALLING FOR DEBIAN\n=====================\n' > $ILOG
+    echo -e '=====================\nINSTALLING FOR DEBIAN\n=====================\n' > "$ILOG"
+
     echo -ne 'Python3\r'
-    sudo apt -y install python3 python3-pip &>> $ILOG && echo 'Python3 - Installed' || 'Python3 - Failed!'
-    echo -e '\n--------------------\n' >> $ILOG
+    sudo apt -y install python3 python3-pip &>> "$ILOG"
+    status_check Python3
+    echo -e '\n--------------------\n' >> "$ILOG"
+
+    echo -ne 'PIP\r'
+    sudo apt -y install python3-pip &>> "$ILOG"
+    status_check Pip
+    echo -e '\n--------------------\n' >> "$ILOG"
+
     echo -ne 'PHP\r'
-    sudo apt -y install php &>> $ILOG && echo 'PHP - Installed' || echo 'PHP - Failed!'
-    echo -e '\n--------------------\n' >> $ILOG
+    sudo apt -y install php &>> "$ILOG"
+    status_check PHP
+    echo -e '\n--------------------\n' >> "$ILOG"
 }
 
 termux_install() {
-    echo -e '=====================\nINSTALLING FOR TERMUX\n=====================\n' > $ILOG
+    echo -e '=====================\nINSTALLING FOR TERMUX\n=====================\n' > "$ILOG"
+
     echo -ne 'Python3\r'
-    apt -y install python &>> $ILOG && echo 'Python3 - Installed' || 'Python3 - Failed!'
-    echo -e '\n--------------------\n' >> $ILOG
+    apt -y install python &>> "$ILOG"
+    status_check Python3
+    echo -e '\n--------------------\n' >> "$ILOG"
+
     echo -ne 'PHP\r'
-    apt -y install php &>> $ILOG && echo 'PHP - Installed' || echo 'PHP - Failed!'
-    echo -e '\n--------------------\n' >> $ILOG
+    apt -y install php &>> "$ILOG"
+    status_check PHP
+    echo -e '\n--------------------\n' >> "$ILOG"
 }
 
 arch_install() {
-    echo -e '=========================\nINSTALLING FOR ARCH LINUX\n=========================\n' > $ILOG
+    echo -e '=========================\nINSTALLING FOR ARCH LINUX\n=========================\n' > "$ILOG"
+
     echo -ne 'Python3\r'
-    yes | sudo pacman -S python3 python-pip --needed &>> $ILOG && echo 'Python3 - Installed' || 'Python3 - Failed!'
-    echo -e '\n--------------------\n' >> $ILOG
+    yes | sudo pacman -S python3 python-pip --needed &>> "$ILOG"
+    status_check Python3
+    echo -e '\n--------------------\n' >> "$ILOG"
+
+    echo -ne 'PIP\r'
+    yes | sudo pacman -S python-pip --needed &>> "$ILOG"
+    status_check Pip
+    echo -e '\n--------------------\n' >> "$ILOG"
+
     echo -ne 'PHP\r'
-    yes | sudo pacman -S php --needed &>> $ILOG && echo 'PHP - Installed' || echo 'PHP - Failed!'
-    echo -e '\n--------------------\n' >> $ILOG
+    yes | sudo pacman -S php --needed &>> "$ILOG"
+    status_check PHP
+    echo -e '\n--------------------\n' >> "$ILOG"
 }
 
 echo -e '[!] Installing Dependencies...\n'
@@ -37,7 +68,7 @@ echo -e '[!] Installing Dependencies...\n'
 if [ -f '/etc/arch-release' ]; then
     arch_install
 else
-    if [ $OSTYPE == 'linux-android' ]; then
+    if [ "$OSTYPE" == 'linux-android' ]; then
         termux_install
     else
         debian_install
@@ -45,9 +76,15 @@ else
 fi
 
 echo -ne 'Requests\r'
-pip3 install requests &>> $ILOG && echo 'Requests - Installed' || echo 'Requests - Failed!'
+pip3 install requests &>> "$ILOG"
+status_check Requests
+echo -e '\n--------------------\n' >> "$ILOG"
 
 echo -ne 'Packaging\r'
-pip3 install packaging &>> $ILOG && echo 'Packaging - Installed' || echo 'Packaging - Failed!'
+pip3 install packaging &>> "$ILOG"
+status_check Packaging
+echo -e '\n--------------------\n' >> "$ILOG"
 
-echo -e '\nLog Saved :' $ILOG
+echo -e '=========\nCOMPLETED\n=========\n' >> "$ILOG"
+
+echo -e '\n[+] Log Saved :' "$ILOG"
